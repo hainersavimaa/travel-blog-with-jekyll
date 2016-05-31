3b7c27fafc968216f924db08b0ada70949dc7f4e
Hainer <hainer@tlu.ee>
2016-05-31 21:20:44 +0300

    Hide postNavigation when post-aside is open

--- js/function.js
+++ js/function.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
    $('.post-aside-cta').click(function(){
-      $('.post-block').toggleClass('post-aside-open')
+      $('.post-block').toggleClass('post-aside-open');
+      $('.PageNavigation').toggleClass('post-aside-open-hidden');
    })
 });
